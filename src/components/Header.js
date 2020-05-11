4eea023f2664a94f90bd6f8849e641086bd658f7
younggg96 <yangguannggeng960123@gmail.com>
2020-05-11 03:28:52 -0400

    add markers on map

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,33 @@
 import React from 'react'
-import { AppBar, Toolbar, Box, Typography } from '@material-ui/core'
+import { AppBar, Toolbar, Box, Typography, IconButton, makeStyles } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
 
 
-
+const useStyles = makeStyles(() => ({
+    title: {
+        flexGrow: 1
+    }
+}))
 
 function Header() {
+    const classes = useStyles()
+
     return (
         <React.Fragment>
             <AppBar color="primary" position="static">
                 <Toolbar>
-                    <Typography variant="h3">
+                    <Typography variant="h3" className={classes.title}>
                         <Box fontWeight="700">
                             Covid 19
                         </Box>
                     </Typography>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <MenuIcon style={{fontSize: 24}}/>
+                    </IconButton>
                 </Toolbar>
             </AppBar>
         </React.Fragment>
--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -3,26 +3,64 @@ import { Grid, Hidden, Typography, Box } from '@material-ui/core';
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useApi } from '../Api/useApi';
+import marker from '../assets/marker/marker.png'
 
 const styles = {
     width: "90vw",
-    height: "50vh"
+    height: "50vh",
+    color: "black"
 };
 
 const getColorCount = cases => {
     if (cases >= 50000) return "red"
     if (cases >= 20000 && cases < 50000) return "blue"
-    if (cases < 20000) return "gray"
+    if (cases <= 20000 && cases > 5000) return "green"
+    if (cases <= 5000) return "gray"
+}
+
+const fatchData = listByCases => {
+    const data = []
+    if (listByCases) {
+        console.log(listByCases)
+        listByCases.slice(0, 90).forEach(({ country, cases, deaths, recovered, countryInfo }, index) => {
+            const { long, lat, flag } = countryInfo
+            data.push({
+                type: "Feature",
+                geometry: {
+                    type: "Point",
+                    coordinates: [long, lat]
+                },
+                properties: {
+                    index,
+                    description:
+                        `<div style="text-align: center">
+                        <img src=${flag} width="80vw" alt=""/>
+                        <h1>${country}</h1>
+                        <h3>Infected: <strong>${cases}</strong></h3>
+                        <h3>Deaths: <strong>${deaths}</strong></h3>
+                        <h3>Recovered: <strong>${recovered}</strong></h3>
+                        </div>
+                        `,
+                    cases: cases,
+                    icon: marker
+                }
+            })
+        })
+    }
+    return Promise.resolve({
+        type: 'geojson',
+        data: {
+            type: "FeatureCollection",
+            features: data
+        }
+    })
 }
 
 function Mapbox() {
     const listByCases = useApi('/countries?sort=cases', null)
     const [map, setMap] = useState(null)
     const mapContainer = useRef(null)
-    const popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false
-    });
+    const hasData = Array.isArray(listByCases) && listByCases.length > 0
     useEffect(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoieWFuZ2dnOTYiLCJhIjoiY2s5a3VsbmkyMXl5eTNubzE1Mnl5emcyaSJ9.1M2DwOkiua7Pl2V_V8ma_A';
         const initializeMap = ({ setMap, mapContainer }) => {
@@ -32,40 +70,41 @@ function Mapbox() {
                 center: [-77.38, 39], // starting position
                 zoom: 1.5 // starting zoom
             });
-            map.on("load", () => {
-                setMap(map)
-                map.resize();
-            })
-        };
-        if (listByCases) {
-            listByCases.forEach(({ country, cases, deaths, recovered, countryInfo }, index) => {
-                const description = `
-                    <strong>${country}</strong>
-                    <p>Cases: ${cases}</p>
-                    <p>Deaths: ${deaths}</p>
-                    <p>Decovered: ${recovered}</p>
-                `;
-                if (cases >= 3000) {
-                    new mapboxgl.Marker({
-                        color: getColorCount(cases)
+            if (hasData) {
+                map.on("load", async () => {
+                    const res = await fatchData(listByCases)
+                    const { data } = res;
+                    const { features } = data;
+                    setMap(map)
+                    map.resize();
+                    features.forEach(({ geometry, properties }) => {
+                        const {cases} = properties
+                        var popup = new mapboxgl.Popup({ offset: 15, closeButton: false, closeOnClick: true })
+                            .setHTML(properties.description)
+                            .setMaxWidth("20vw")
+                        new mapboxgl.Marker({ color: getColorCount(cases) })
+                            .setLngLat(geometry.coordinates)
+                            .setPopup(popup)
+                            .addTo(map);
                     })
-                        .setLngLat([countryInfo.long, countryInfo.lat])
-                        .addTo(map);
-                    map.on('mouseenter', 'places', function () {
-                        map.getCanvas().style.cursor = 'pointer';
-                        popup
-                            .setHTML(description)
-                            .addTo(map)
-                    })
-                }
-            });
-        }
+                });
+            }
 
+        };
         if (!map) initializeMap({ setMap, mapContainer });
 
-    }, [map])
+    }, [map, hasData, listByCases])
+    if (hasData) {
+        return <div ref={el => (mapContainer.current = el)} style={styles} />
+    }
 
-    return <div ref={el => (mapContainer.current = el)} style={styles} />
+    return (<div ref={el => (mapContainer.current = el)} style={styles}>
+        <Typography variant="h2" color="white" style={{ paddingLeft: "32px", textAlign: "center" }}>
+            <Box fontWeight={300}>
+                Loading...
+            </Box>
+        </Typography>
+        </div>)
 
 }
 
